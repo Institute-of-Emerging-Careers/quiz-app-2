252b89a20b99f6972461a23be08457f21b393e95
hallucinogenizer <22100063@lums.edu.pk>
2022-01-15 17:39:33 +0500

    fixed payload too large error

--- server.js
+++ server.js
@@ -102,9 +102,9 @@ require("dotenv").config();
 
 // Middleware
 app.set("view-engine", "ejs");
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.static("resources"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(flash());
 app.use(cookieParser());
 app.use(
