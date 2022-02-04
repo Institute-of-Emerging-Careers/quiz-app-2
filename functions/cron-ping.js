29dce08b23cda0cfb92dfae18ff36a8977c3f9b0
hallucinogenizer <22100063@lums.edu.pk>
2022-02-05 00:41:50 +0500

    merged cron-jobs to main process

--- functions/cron-ping.js
+++ functions/cron-ping.js
@@ -6,10 +6,10 @@ const cron = require('node-cron');
     Answer,
     Attempt,
     PasswordResetLink
-  } = require("./db/models/user");
-const scoreAssignmentsWhoseDeadlineHasPassed = require('./functions/scoreAssignmentsWhoseDeadlineHasPassed');
-const scoreAttemptsWhoseTimerHasEnded = require('./functions/scoreAttemptsWhoseTimerHasEnded');
-const sendReminderEmails = require('./functions/sendReminderEmails');
+  } = require("../db/models/user");
+const scoreAssignmentsWhoseDeadlineHasPassed = require('./scoreAssignmentsWhoseDeadlineHasPassed');
+const scoreAttemptsWhoseTimerHasEnded = require('./scoreAttemptsWhoseTimerHasEnded');
+const sendReminderEmails = require('./sendReminderEmails');
 
 const assessment_reminder_mailer_task = cron.schedule('0 0 */3 * * *', () => {
   console.log('Running 3-hourly reminder email.');
@@ -26,5 +26,4 @@ const score_past_deadline_attempts = cron.schedule('0 */3 * * * *', async () => 
   scheduled: false
 })
 
-assessment_reminder_mailer_task.start();
-score_past_deadline_attempts.start()
+module.exports = {assessment_reminder_mailer_task, score_past_deadline_attempts}
