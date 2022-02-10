8e2c9f660266e540bbdc37c3243fdc16423c6dba
hallucinogenizer <22100063@lums.edu.pk>
2022-02-10 14:48:02 +0500

    fixed empty gender,city,age,address bug on signup

--- resources/js/student-signup.js
+++ resources/js/student-signup.js
@@ -1,29 +1,64 @@
 function validateFirstname(firstname) {
-  if (firstname.length > 100) return "First name too long. Cannot exceed 60 alphabets.";
-  else if (firstname.length < 1) return "First name too short. Must be at least 1 character.";
+  if (firstname.length > 100)
+    return "First name too long. Cannot exceed 60 alphabets.";
+  else if (firstname.length < 1)
+    return "First name too short. Must be at least 1 character.";
   else return true;
 }
 function validateLastname(lastname) {
-  if (lastname.length > 100) return "Last name too long. Cannot exceed 30 alphabets.";
-  else if (lastname.length < 1) return "Last name too short. Must be at least 1 character.";
+  if (lastname.length > 100)
+    return "Last name too long. Cannot exceed 30 alphabets.";
+  else if (lastname.length < 1)
+    return "Last name too short. Must be at least 1 character.";
   return true;
 }
 function validateEmail(email) {
-  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!regex.test(String(email).toLowerCase())) return "Email address invalid. Please enter a valid email address.";
-  else if (email.length > 254) return "Email address too long. Cannot exceed 254 characters.";
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!regex.test(String(email).toLowerCase()))
+    return "Email address invalid. Please enter a valid email address.";
+  else if (email.length > 254)
+    return "Email address too long. Cannot exceed 254 characters.";
 
   return true;
 }
 function validateCNIC(cnic) {
   const regex = /\d\d\d\d\d-\d\d\d\d\d\d\d-\d/i;
-  if (!regex.test(cnic)) return "CNIC format invalid. CNIC must contain 13 digits with two dashes (-) in between e.g. 35201-1234567-8";
+  if (!regex.test(cnic))
+    return "CNIC format invalid. CNIC must contain 13 digits with two dashes (-) in between e.g. 35201-1234567-8";
   return true;
 }
 function validatePhone(phone) {
   const regex = /\d\d\d\d\d\d\d\d\d\d\d/i;
-  if (!regex.test(phone)) return "Phone number invalid. Phone number must be 11 digits long without any dashes, spaces, or symbols e.g. 03001234567";
+  if (!regex.test(phone))
+    return "Phone number invalid. Phone number must be 11 digits long without any dashes, spaces, or symbols e.g. 03001234567";
   return true;
+}
+
+function validateGender(gender) {
+  if (gender != "male" && gender != "female" && gender != "other")
+    return "Gender invalid. Please select a gender.";
+  return true;
+}
+
+function validateCity(city) {
+  if (city.length < 2 || city.length > 100) {
+    return "City name must be at least 2 and at most 100 characters long.";
+  } else {
+    return true;
+  }
+}
+
+function validateAddress(address) {
+  if (address.length < 10 || address.length > 300) {
+    return "Full address must at least be 10 characters and at most 300 characters long.";
+  } else return true;
+}
+
+function validateAge(age) {
+  if (age < 10 || age > 100) {
+    return "Age must be between 10 and 100";
+  } else return true;
 }
 
 function displayError(field, error) {
@@ -70,8 +105,8 @@ $(document).ready(function () {
   let cnic_mirror = $("#cnic-mirror");
   let gender_mirror = $("#gender-mirror");
   let age_mirror = $("#age-mirror");
-  let city_mirror = $("#city-mirror")
-  let address_mirror = $("#address-mirror")
+  let city_mirror = $("#city-mirror");
+  let address_mirror = $("#address-mirror");
   let back_arrow = $("#back-arrow");
 
   let firstname_field = $("#firstName");
@@ -81,58 +116,71 @@ $(document).ready(function () {
   let phone_field = $("#phone");
   let gender_field = $("#gender");
   let age_field = $("#age");
-  let city_field = $("#city")
-  let address_field = $("#address")
+  let city_field = $("#city");
+  let address_field = $("#address");
   let password_field = $("#password");
   let password_retype = $("#password2");
 
   let name_to_field_obj = {
-    'firstName':firstname_field,
-    'lastName':lastname_field,
-    'email':email_field,
-    'cnic':cnic_field,
-    'phone':phone_field,
-    'gender':gender_field,
-    'age':age_field,
-    'city':city_field,
-    'address':address_field,
-    'password':password_field
-  }
+    firstName: firstname_field,
+    lastName: lastname_field,
+    email: email_field,
+    cnic: cnic_field,
+    phone: phone_field,
+    gender: gender_field,
+    age: age_field,
+    city: city_field,
+    address: address_field,
+    password: password_field,
+  };
 
   // getting GET search parameters from URL and converting them to a JavaScript object
   function paramsToObject(entries) {
-    const result = {}
-    for(const [key, value] of entries) { // each 'entry' is a [key, value] tupple
+    const result = {};
+    for (const [key, value] of entries) {
+      // each 'entry' is a [key, value] tupple
       result[key] = value;
     }
     return result;
   }
-  
+
   let search = location.search.substring(1);
   const urlParams = new URLSearchParams(search);
   const entries = urlParams.entries(); //returns an iterator of decoded [key,value] tuples
   const searchParams = paramsToObject(entries); //{abc:"foo",def:"[asf]",xyz:"5"}
 
-
   if (searchParams.hasOwnProperty("error")) {
     if (searchParams["error"] == "Validation error") {
-      console.log(name_to_field_obj[searchParams["field"]])
-      displayError(name_to_field_obj[searchParams["field"]], searchParams["message"])
+      console.log(name_to_field_obj[searchParams["field"]]);
+      displayError(
+        name_to_field_obj[searchParams["field"]],
+        searchParams["message"]
+      );
     }
   }
 
   // checking if the email entered is already taken, if yes we redirect user to login page
-  email_field.focusout((e)=>{
-    $.get("/email-exists/" + email_field.val(), (data, success)=>{
-      if (data == true) window.location="/student/login?link=" + location.pathname.split('/').slice(-1)[0] + "&email=" + encodeURIComponent(email_field.val())
-    })
-  })
+  email_field.focusout((e) => {
+    $.get("/email-exists/" + email_field.val(), (data, success) => {
+      if (data == true)
+        window.location =
+          "/student/login?link=" +
+          location.pathname.split("/").slice(-1)[0] +
+          "&email=" +
+          encodeURIComponent(email_field.val());
+    });
+  });
 
-  cnic_field.focusout((e)=>{
-    $.get("/cnic-exists/" + cnic_field.val(), (data, success)=>{
-      if (data == true) window.location="/student/login?link=" + location.pathname.split('/').slice(-1)[0] + "&cnic=" + encodeURIComponent(cnic_field.val())
-    })
-  })
+  cnic_field.focusout((e) => {
+    $.get("/cnic-exists/" + cnic_field.val(), (data, success) => {
+      if (data == true)
+        window.location =
+          "/student/login?link=" +
+          location.pathname.split("/").slice(-1)[0] +
+          "&cnic=" +
+          encodeURIComponent(cnic_field.val());
+    });
+  });
 
   $("#section1-button").click(function () {
     // getting the values
@@ -147,7 +195,17 @@ $(document).ready(function () {
     let address = address_field.val().toUpperCase();
 
     // reseting all errors
-    resetAllErrors([firstname_field, lastname_field, email_field, cnic_field, phone_field]);
+    resetAllErrors([
+      firstname_field,
+      lastname_field,
+      email_field,
+      cnic_field,
+      phone_field,
+      gender_field,
+      age_field,
+      city_field,
+      address_field,
+    ]);
 
     // validating input
     let firstname_validation = validateFirstname(firstname);
@@ -155,6 +213,10 @@ $(document).ready(function () {
     let email_validation = validateEmail(email);
     let cnic_validation = validateCNIC(cnic);
     let phone_validation = validatePhone(phone);
+    let gender_validation = validateGender(gender);
+    let age_validation = validateAge(age);
+    let city_validation = validateCity(city);
+    let address_validation = validateAddress(address);
 
     if (firstname_validation !== true) {
       displayError(firstname_field, firstname_validation);
@@ -166,6 +228,14 @@ $(document).ready(function () {
       displayError(cnic_field, cnic_validation);
     } else if (phone_validation !== true) {
       displayError(phone_field, phone_validation);
+    } else if (gender_validation !== true) {
+      displayError(gender_field, gender_validation);
+    } else if (age_validation !== true) {
+      displayError(age_field, age_validation);
+    } else if (city_validation !== true) {
+      displayError(city_field, city_validation);
+    } else if (address_validation !== true) {
+      displayError(address_field, address_validation);
     } else {
       section1.slideToggle();
       curScreen = !curScreen;
@@ -184,7 +254,13 @@ $(document).ready(function () {
 
   // the back arrow
   back_arrow.click(function () {
-    resetAllErrors([firstname_field, lastname_field, email_field, cnic_field, phone_field]);
+    resetAllErrors([
+      firstname_field,
+      lastname_field,
+      email_field,
+      cnic_field,
+      phone_field,
+    ]);
     section1.slideToggle();
     curScreen = !curScreen;
     section2.slideToggle();
@@ -198,8 +274,14 @@ $(document).ready(function () {
       e.stopPropagation();
       resetAllErrors([password_field, password_retype]);
       if (password_field.val() != password_retype.val()) {
-        displayError(password_field, "Please make sure that the passwords written in these two fields match.");
-        displayError(password_retype, "Please make sure that the passwords written in these two fields match.");
+        displayError(
+          password_field,
+          "Please make sure that the passwords written in these two fields match."
+        );
+        displayError(
+          password_retype,
+          "Please make sure that the passwords written in these two fields match."
+        );
       } else {
         final_submission = true;
         $("#form1").submit();
