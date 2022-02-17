91ab83b8a4939f7aa74982501607de7c63f8339d
hallucinogenizer <22100063@lums.edu.pk>
2022-02-17 23:36:13 +0500

    disabled sequelize query logging

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require("sequelize");
-console.log(process.env.DEBUG)
+console.log(process.env.DEBUG);
 
 const sequelize = new Sequelize(process.env.MYSQL_CONNECTION_STRING, {
   dialect: "mysql",
-  timezone:process.env.TIMEZONE_OFFSET,
+  logging: false,
+  timezone: process.env.TIMEZONE_OFFSET,
 });
 
 sequelize
