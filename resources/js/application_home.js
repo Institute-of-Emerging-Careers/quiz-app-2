14791009d6eecd40135cc20e0c81741b1ff7e6f0
hallucinogenizer <22100063@lums.edu.pk>
2022-05-19 04:37:16 +0500

    creating of new application round complete

--- resources/js/application_home.js
+++ resources/js/application_home.js
@@ -4,34 +4,98 @@ const useState = React.useState;
 
 const App = () => {
   const [application_rounds, setApplicationRounds] = useState([]);
+  const [courses, setCourses] = useState([]);
+  const [new_round_title, setNewRoundTitle] = useState("");
+  const [new_course_title, setNewCourseTitle] = useState("");
+  const [show_modal, setShowModal] = useState(false);
+  const [update_data, setUpdateData] = useState(0);
+  const [show_copied_box, setShowCopiedBox] = useState(false);
 
   useEffect(() => {
-    fetch("/admin/application/all-rounds")
+    fetch("/admin/application/rounds/all")
       .then((raw_response) => {
-        raw_response
-          .json()
-          .then((response) => {
-            if (response.success)
+        if (!raw_response.ok) {
+          alert(
+            "Something went wrong while getting application rounds. Error code 01."
+          );
+        } else {
+          raw_response
+            .json()
+            .then((response) => {
               setApplicationRounds(response.application_rounds);
-            else
-              alert(
-                "Something went wrong while getting interview rounds. Error code 01."
+              setCourses(
+                response.courses.map((course) => {
+                  course.checked = false;
+                  return course;
+                })
               );
-          })
-          .catch((err) => {
-            alert(
-              "Error while understanding the server's response. Error code 02."
-            );
-          });
+            })
+            .catch((err) => {
+              console.log(err);
+              alert(
+                "Error while understanding the server's response. Error code 02."
+              );
+            });
+        }
       })
       .catch((err) => {
         alert(
           "Please check your internet connection and try again. Error code 03."
         );
       });
-  }, []);
+  }, [update_data]);
 
-  const createApplicationRound = () => {};
+  const createApplicationRound = (e) => {
+    e.preventDefault();
+    if (new_round_title == "")
+      alert("Please give the Application Round a title.");
+    else if (
+      courses.reduce((all_false, cur) => {
+        if (!all_false) return false;
+        if (cur.checked) return false;
+        else return true;
+      }, true)
+    )
+      alert("Please select at least 1 course.");
+    else {
+      fetch("/admin/application/rounds/new", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: new_round_title,
+          courses: courses,
+        }),
+      }).then((response) => {
+        if (response.ok) {
+          setShowModal(false);
+          setUpdateData((cur) => cur + 1);
+        } else alert("Could not create application round.");
+      });
+    }
+  };
+
+  const addNewCourse = () => {
+    fetch("/admin/application/course/new", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: new_course_title,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          setCourses((cur) => [
+            ...cur,
+            { title: new_course_title, checked: false },
+          ]);
+          setNewCourseTitle("");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Something went wrong while adding a new course.");
+      });
+  };
 
   return (
     <div>
@@ -58,15 +122,71 @@ const App = () => {
             ></i>
           </div>
           <div className="p-8">
-            <p>Please enter the name of this Application Round.</p>
             <form onSubmit={createApplicationRound}>
+              <label htmlFor="title">
+                Please give this Application Round a title:
+              </label>
               <input
                 type="text"
                 min="2"
+                name="title"
                 placeholder="Cohort 4 Application Round 1"
-                className="px-4 py-2"
+                className="border px-4 py-2 ml-2"
+                value={new_round_title}
+                onChange={(e) => {
+                  setNewRoundTitle(e.target.value);
+                }}
               ></input>
-              <input type="submit" value="Create Application Round"></input>
+              <br></br>
+
+              <div className="mt-4">
+                <label htmlFor="courses">
+                  Which courses can students apply for?
+                </label>
+                {courses.map((course, index) => (
+                  <div>
+                    <input
+                      type="checkbox"
+                      name="courses"
+                      checked={course.checked}
+                      data-index={index}
+                      onChange={(e) => {
+                        setCourses((cur) => {
+                          let copy = cur.slice();
+                          copy[e.target.dataset.index].checked =
+                            !copy[e.target.dataset.index].checked;
+                          return copy;
+                        });
+                      }}
+                    ></input>
+                    <label>{" " + course.title}</label>
+                  </div>
+                ))}
+                <input type="checkbox" name="courses"></input>
+                <input
+                  type="text"
+                  name="newCourseTitle"
+                  className="py-1 px-2 border"
+                  placeholder="Course Title"
+                  value={new_course_title}
+                  onChange={(e) => {
+                    setNewCourseTitle(e.target.value);
+                  }}
+                ></input>
+                <button
+                  type="button"
+                  onClick={addNewCourse}
+                  className="px-2 py-1 bg-gray-400"
+                >
+                  Add New Course
+                </button>
+              </div>
+              <br></br>
+              <input
+                type="submit"
+                value="Create Application Round"
+                className="px-4 py-2 mt-4 bg-iec-blue hover:bg-iec-blue-hover text-white"
+              ></input>
             </form>
           </div>
         </div>
@@ -75,7 +195,7 @@ const App = () => {
       <h2 className="text-xl mt-6 mb-4 font-bold">
         Applications
         <button
-          className="text-xs px-4 py-1 cursor-pointer bg-iec-blue hover:bg-iec-blue-hover text-white rounded-full"
+          className="text-xs px-4 ml-2 py-1 cursor-pointer bg-iec-blue hover:bg-iec-blue-hover text-white rounded-full"
           onClick={() => {
             setShowModal((cur) => !cur);
           }}
@@ -84,34 +204,64 @@ const App = () => {
         </button>
       </h2>
       <div className="flex flex-wrap justify-start gap-y-10 gap-x-10">
-        {interview_rounds.map((interview_round, index) => (
-          <div
-            className="grid w-64 grid-cols-6 gap-4 border bg-white pb-2 quiz-card"
-            key={index}
-          >
-            <div className="grid grid-cols-2 col-span-8 h-16 bg-iec-blue justify-center content-center">
-              <a
-                href={`/admin/interview/edit/${interview_round.id}`}
-                className="text-white text-xl col-span-1 self-center justify-self-center hover:text-gray-100 cursor-pointer"
-                title="Edit Interview Round"
-              >
-                <i className="far fa-edit "></i>
-              </a>
-              <a
-                className="text-white text-xl col-span-1 self-center justify-self-center hover:text-gray-100 cursor-pointer"
-                title="Delete Interview Round"
-              >
-                <i className="fas fa-trash "></i>
-              </a>
+        {application_rounds.length == 0 ? (
+          <p>No application rounds to show.</p>
+        ) : (
+          application_rounds.map((application_round, index) => (
+            <div
+              className="grid w-64 grid-cols-6 gap-4 border bg-white pb-2 quiz-card"
+              key={index}
+            >
+              <div className="grid grid-cols-5 col-span-6 h-16 bg-iec-blue justify-center content-center">
+                <a
+                  href={`/admin/application/edit/${application_round.id}`}
+                  className="text-white text-xl col-start-2 justify-self-center col-span-1 hover:text-gray-100 cursor-pointer"
+                  title="Edit Application Round"
+                >
+                  <i className="far fa-edit "></i>
+                </a>
+                <a
+                  data-id={application_round.id}
+                  onClick={(e) => {
+                    navigator.clipboard.writeText(
+                      `${site_domain_name}/application/${e.target.dataset.id}`
+                    );
+                    setShowCopiedBox(true);
+                    window.setTimeout(() => {
+                      setShowCopiedBox(false);
+                    }, 2000);
+                  }}
+                  className="text-white text-xl col-span-1 justify-self-center hover:text-gray-100 cursor-pointer relative"
+                  title="Copy Link"
+                >
+                  <i className="fas fa-link" data-id={application_round.id}></i>
+                  {show_copied_box ? (
+                    <div
+                      className="text-xs absolute left-2 top-2 w-max bg-white px-4 py-2 shadow-md text-gray-800"
+                      data-id={application_round.id}
+                    >
+                      Linked Copied to Clipboard!
+                    </div>
+                  ) : (
+                    <span></span>
+                  )}
+                </a>
+                <a
+                  className="text-white text-xl col-span-1 justify-self-center hover:text-gray-100 cursor-pointer"
+                  title="Delete Application Round"
+                >
+                  <i className="fas fa-trash "></i>
+                </a>
+              </div>
+              <h3 className="col-span-6 font-semibold text-lg px-4">
+                {application_round.title}
+              </h3>
+              <div className="col-start-1 col-span-6">
+                <p className="pl-4 pt-0">0 applied</p>
+              </div>
             </div>
-            <h3 className="col-span-6 font-semibold text-lg px-4">
-              {interview_round.title}
-            </h3>
-            <div className="col-start-1 col-span-6">
-              <p className="pl-4 pt-0">0 applied</p>
-            </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
--- routes/application.js
+++ routes/application.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const {
+  ApplicationRound,
+  Course,
+  ApplicationRoundCourseJunction,
+} = require("../db/models/application");
 
 const checkAdminAuthenticated = require("../db/check-admin-authenticated");
 
@@ -18,6 +23,45 @@ router.get("/", checkAdminAuthenticated, (req, res) => {
   });
 });
 
-router.get("/all-rounds", checkAdminAuthenticated, (req, res) => {});
+router.get("/rounds/all", checkAdminAuthenticated, async (req, res) => {
+  res.json({
+    application_rounds: await ApplicationRound.findAll(),
+    courses: await Course.findAll({ attributes: ["id", "title"] }),
+  });
+});
+
+router.post("/course/new", checkAdminAuthenticated, async (req, res) => {
+  try {
+    await Course.create(req.body);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(501);
+  }
+});
+
+router.post("/rounds/new", checkAdminAuthenticated, async (req, res) => {
+  const new_application_round = await ApplicationRound.create({
+    title: req.body.title,
+  });
+
+  Promise.all(
+    req.body.courses.map((course) =>
+      ApplicationRoundCourseJunction.findOrCreate({
+        where: {
+          ApplicationRoundId: new_application_round.id,
+          CourseId: course.id,
+        },
+      })
+    )
+  )
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(501);
+    });
+});
 
 module.exports = router;
