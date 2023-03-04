7a7f7e01c30044cb6b4c4708d81d0c965b064c7a
rohan-hussain <muhammad.rohan@educative.io>
2023-03-04 21:35:25 +0500

    grouped jest tests in describe block

--- jest-tests/unit.test.js
+++ jest-tests/unit.test.js
@@ -21,22 +21,23 @@ jest.mock('../db/models', () => {
 		},
 	}
 })
+describe('Test functions/allSectionsSolved.js', () => {
+	beforeAll(() => sequelize.sync({ alter: true, force: true }))
+	afterAll(() => sequelize.close())
 
-beforeAll(() => sequelize.sync({ alter: true, force: true }))
-afterAll(() => sequelize.close())
+	test('should return true if assignment was completed', async () => {
+		const assignment = { completed: true }
+		expect(allSectionsSolved(0, assignment)).resolves.toBeTruthy()
+	})
 
-test('allSectionsSolved function should return true if assignment was completed', async () => {
-	const assignment = { completed: true }
-	expect(allSectionsSolved(0, assignment)).resolves.toBeTruthy()
-})
+	test('should return true if all', async () => {
+		const assignment = { completed: false }
+		expect(allSectionsSolved(0, assignment)).resolves.toBeTruthy()
+	})
 
-test('allSectionsSolved function should return true if all', async () => {
-	const assignment = { completed: false }
-	expect(allSectionsSolved(0, assignment)).resolves.toBeTruthy()
-})
-
-test('allSectionsSolved function should return false if a section has no attempts', async () => {
-	const assignment = { completed: false }
-	mockAttemptResponse = null
-	expect(allSectionsSolved(0, assignment)).resolves.toBeFalsy()
+	test('should return false if a section has no attempts', async () => {
+		const assignment = { completed: false }
+		mockAttemptResponse = null
+		expect(allSectionsSolved(0, assignment)).resolves.toBeFalsy()
+	})
 })
