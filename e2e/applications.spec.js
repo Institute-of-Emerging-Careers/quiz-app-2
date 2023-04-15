7022fd5c1ce07625f37f033c7d06b9b393ff2109
Rohan Hussain <22100063@lums.edu.pk>
2023-04-15 23:59:27 +0500

    progressed to application form opening

--- e2e/applications.spec.js
+++ e2e/applications.spec.js
@@ -1,11 +1,26 @@
 const { test, expect } = require("@playwright/test");
 require("dotenv").config();
 const randomstring = require("randomstring");
-import { logIntoAdminPanel } from "./utils";
+import { logIntoAdminPanel, createCourseAndCheck, logout } from "./utils/application/utils";
 
-test("Create Application Round", async ({ page }) => {
+let applicationRoundId = null, roundTitle = `E2E Test Round ${randomstring.generate(8)}`, applicationFormLink = '';
+
+// this tells Playwright to run these tests one after the other, and if one test fails, skip the others
+test.describe.configure({ mode: 'serial' });
+
+let page;
+
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage();
   await logIntoAdminPanel(page);
+});
 
+test.afterAll(async () => {
+  await page.close();
+});
+
+test("Create Application Round", async () => {
   // Create Application Round
   await page.getByRole("link", { name: /Applications/ }).click();
   await expect(page).toHaveTitle("Cohort Applications - Admin Panel");
@@ -15,45 +30,33 @@ test("Create Application Round", async ({ page }) => {
     name: "Create Application Round",
   });
   await expect(createRoundButton).toBeVisible();
-  const roundTitle = `E2E Test Round ${randomstring.generate(8)}`;
   await page.getByPlaceholder("Cohort 4 Application Round 1").fill(roundTitle);
-  let title = "Course A"
 
-  // Create Course A and make sure it exists
-  await page.getByPlaceholder("Course Title").fill(title);
-  let addCourseButton = await page.getByRole("button", {
-    name: "Add New Course",
-  });
-  await addCourseButton.click();
-  await page.locator(`[data-testid="checkbox-${title}"]`).check();
-  let isChecked = await page.locator(`[data-testid="checkbox-${title}"]`).isChecked()
-  expect(isChecked).toBeTruthy();
-
-  // Create Course B and make sure it exists
-  title = "Course B"
-  await page.getByPlaceholder("Course Title").fill(title);
-  addCourseButton = await page.getByRole("button", {
-    name: "Add New Course",
-  });
-  await addCourseButton.click();
-  await page.locator(`[data-testid="checkbox-${title}"]`).check();
-  isChecked = await page.locator(`[data-testid="checkbox-${title}"]`).isChecked()
-  expect(isChecked).toBeTruthy();
-
-  // Create Course C and make sure it exists
-  title = "Course C"
-  await page.getByPlaceholder("Course Title").fill(title);
-  addCourseButton = await page.getByRole("button", {
-    name: "Add New Course",
-  });
-  await addCourseButton.click();
-  await page.locator(`[data-testid="checkbox-${title}"]`).check();
-  isChecked = await page.locator(`[data-testid="checkbox-${title}"]`).isChecked()
-  expect(isChecked).toBeTruthy();
+  // Create three courses and check them
+  await createCourseAndCheck("Course A", page)
+  await createCourseAndCheck("Course B", page)
+  await createCourseAndCheck("Course C", page)
 
   // Create the Application Round
   await createRoundButton.click();
 
   // Make sure the round was successfully created
   await expect(page.getByText(roundTitle)).toBeVisible();
+
+  // Copy the url to the application form
+  const copyUrlButton = await page.getByTestId(`copy-${roundTitle}-link`)
+  applicationRoundId = await copyUrlButton.getAttribute('data-id')
+  console.log("applicationRoundId:", applicationRoundId)
+  applicationFormLink = `http://localhost:${process.env.PORT}/application/fill/${applicationRoundId}`
 });
+
+test("Go to Application Form", async () => {
+  await logout(page);
+
+  // Go to application form as logged out student
+  await page.goto(applicationFormLink);
+
+  // Select the h1 element by looking for its text content
+  await expect(page.getByRole('heading', { name: 'Apply to IEC' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Step 1: Basic Information *' })).toBeVisible();
+})
---
+++ e2e/utils/application/utils.js
@@ -0,0 +1,31 @@
+const { expect } = require("@playwright/test");
+
+export const logIntoAdminPanel = async (page) => {
+  await page.goto(`/admin`);
+  await expect(page).toHaveTitle("Login - Admin Panel");
+
+  await page.getByLabel("Email Address:").fill("admin@iec.org.pk");
+  await page.getByLabel("Password:").fill("examplepassword");
+  await page.getByRole("button", { name: "Log In" }).click();
+  await page.waitForLoadState('load');
+  return expect(page).toHaveTitle("Home - Admin Panel");
+};
+
+export const createCourseAndCheck = async (title, page) => {
+  await page.getByPlaceholder("Course Title").fill(title);
+  let addCourseButton = await page.getByRole("button", {
+    name: "Add New Course",
+  });
+  await addCourseButton.click();
+  await page.locator(`[data-testid="checkbox-${title}"]`).check();
+  let isChecked = await page.locator(`[data-testid="checkbox-${title}"]`).isChecked()
+  return expect(isChecked).toBeTruthy();
+}
+
+export const logout = async (page) => {
+  const logoutLink = await page.waitForSelector('a[href="/logout"]');
+  await logoutLink.click();
+
+  // Wait for second redirect to /admin/login
+  return page.waitForURL(`http://localhost:${process.env.PORT}/admin/login`);
+}
