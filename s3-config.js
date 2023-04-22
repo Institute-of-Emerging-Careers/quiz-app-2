65e9983c3af1bd4ac49ea29748e88d1d996aa670
Rohan Hussain <22100063@lums.edu.pk>
2023-04-22 19:04:45 +0500

    attempt at fixing bucket_name bug

--- s3-config.js
+++ s3-config.js
@@ -2,6 +2,7 @@ const { S3Client } = require('@aws-sdk/client-s3')
 
 const s3 = new S3Client({
     region: process.env.AWS_REGION,
+    bucket: process.env.LEC_BUCKET_NAME,
 })
 
 module.exports = s3
