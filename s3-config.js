b37c6adc27c11c5025dc8e1aa1b26cd7ebccbdaf
Rohan Hussain <22100063@lums.edu.pk>
2023-04-22 19:17:09 +0500

    attempt-3

--- s3-config.js
+++ s3-config.js
@@ -1,12 +1,7 @@
 const { S3Client } = require('@aws-sdk/client-s3')
 
-let s3;
-if (process.env.NODE_ENV !== 'test') {
-    s3 = new S3Client({
-        region: process.env.AWS_REGION,
-    })
-} else {
-    s3 = {}
-}
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+})
 
 module.exports = s3
